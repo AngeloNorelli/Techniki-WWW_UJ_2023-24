87ac363f2e61ae1a2303aa31bd3d720a11d13e9b
AngeloNorelli <angnorelli@gmail.com>
2023-11-29 23:38:58 +0100

    Zarys projektu

---
+++ Projekty Zaliczeniowe/Projekt 1/script.js
@@ -0,0 +1,48 @@
+document.addEventListener('click', function(event) {
+    const allSubmenus = document.querySelectorAll('.submenu');
+    const allTabs = document.querySelectorAll('.tab');
+
+    let clickedOnTab = false;
+
+    allTabs.forEach(function(tab) {
+        if (tab.contains(event.target)) {
+            clickedOnTab = true;
+        }
+    });
+
+    if (!clickedOnTab) {
+        allSubmenus.forEach(function(submenu) {
+            submenu.style.display = 'none';
+        });
+    }
+});
+
+function toggleSubmenu(submenuID) {
+    const submenu = document.getElementById(submenuID);
+    const allSubmenus = document.querySelectorAll('.submenu');
+
+    allSubmenus.forEach(function(item) {
+        if (item.id !== submenuID) {
+            item.style.display = 'none';
+        }
+    });
+
+    if(submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+}
+
+function showSubpage(pageID) {
+    const page = document.getElementById(pageID);
+    const allPages = document.querySelectorAll('.content');
+
+    allPages.forEach(function(item) {
+        if (item !== page) {
+            item.style.display = 'none';
+        } else {
+            item.style.display = 'block'
+        }
+    });
+}
